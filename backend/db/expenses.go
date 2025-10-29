f30e28f004f133d832b8c4a1993d376c9769e3a7
Pranaov S <148376495+pranaovs@users.noreply.github.com>
2025-10-29 21:33:43 +0530

    feat: add expense db functions

---
+++ backend/db/expenses.go
@@ -0,0 +1,160 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"shared-expenses-app/models"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateExpense(
+	ctx context.Context,
+	pool *pgxpool.Pool,
+	expense models.Expense,
+) (models.Expense, error) {
+	if expense.Title == "" {
+		return models.Expense{}, errors.New("title required")
+	}
+	if expense.Amount <= 0 {
+		return models.Expense{}, errors.New("invalid amount")
+	}
+
+	var exp models.Expense
+	err := pool.QueryRow(
+		ctx,
+		`INSERT INTO expenses (
+			group_id, added_by, title, description, amount,
+			is_incomplete_amount, is_incomplete_split, latitude, longitude, created_at
+		)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+		RETURNING expense_id, group_id, added_by, title, description,
+		          extract(epoch from created_at)::bigint, amount, is_incomplete_amount, is_incomplete_split,
+		          latitude, longitude`,
+		expense.GroupID,
+		expense.AddedBy,
+		expense.Title,
+		expense.Description,
+		expense.Amount,
+		expense.IsIncompleteAmount,
+		expense.IsIncompleteSplit,
+		expense.Latitude,
+		expense.Longitude,
+		time.Now(),
+	).Scan(
+		&exp.ExpenseID,
+		&exp.GroupID,
+		&exp.AddedBy,
+		&exp.Title,
+		&exp.Description,
+		&exp.CreatedAt,
+		&exp.Amount,
+		&exp.IsIncompleteAmount,
+		&exp.IsIncompleteSplit,
+		&exp.Latitude,
+		&exp.Longitude,
+	)
+	if err != nil {
+		return models.Expense{}, err
+	}
+
+	return exp, nil
+}
+
+func UpdateExpense(ctx context.Context, pool *pgxpool.Pool, expense models.Expense) error {
+	if expense.Title == "" {
+		return errors.New("title required")
+	}
+	if expense.Amount <= 0 {
+		return errors.New("invalid amount")
+	}
+	err := pool.QueryRow(
+		ctx,
+		`UPDATE expenses
+			SET title = COALESCE($2, title),
+			description = COALESCE($3, description),
+			amount = COALESCE($4, amount),
+			added_by = COALESCE($5, added_by),
+			latitude = COALESCE($6, latitude),
+			longitude = COALESCE($7, longitude)
+			WHERE expense_id = $1
+			RETURNING expense_id, group_id, added_by, title, description,
+			extract(epoch from created_at)::bigint, amount,
+			is_incomplete_amount, is_incomplete_split, latitude, longitude`,
+
+		expense.ExpenseID,
+		expense.GroupID,
+		expense.Title,
+		expense.Description,
+		expense.Amount,
+		expense.IsIncompleteAmount,
+		expense.IsIncompleteSplit,
+		expense.Latitude,
+		expense.Longitude,
+	).Scan()
+
+	if err == pgx.ErrNoRows {
+		return errors.New("expense not found")
+	}
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetExpense(ctx context.Context, pool *pgxpool.Pool, expenseID string) (models.Expense, error) {
+	var expense models.Expense
+	err := pool.QueryRow(
+		ctx,
+		`SELECT expense_id,
+			group_id,
+			added_by,
+			title,
+			description,
+			extract(epoch from created_at)::bigint,
+			amount,
+			is_incomplete_amount,
+			is_incomplete_split,
+			latitude,
+			longitude
+			WHERE expense_id = $1`,
+		expenseID,
+	).Scan(
+		&expense.ExpenseID,
+		expense.GroupID,
+		expense.AddedBy,
+		expense.Title,
+		expense.Description,
+		expense.CreatedAt,
+		expense.Amount,
+		expense.IsIncompleteAmount,
+		expense.IsIncompleteSplit,
+		expense.Latitude,
+		expense.Longitude,
+	)
+	if err == pgx.ErrNoRows {
+		return models.Expense{}, errors.New("expense not found")
+	}
+	if err != nil {
+		return models.Expense{}, err
+	}
+
+	return expense, nil
+}
+
+func DeleteExpense(ctx context.Context, pool *pgxpool.Pool, expenseID, userID string) error {
+	cmd, err := pool.Exec(ctx, `DELETE FROM expenses WHERE expense_id = $1`, expenseID)
+	if err != nil {
+		return err
+	}
+
+	if cmd.RowsAffected() == 0 {
+		return errors.New("no rows deleted")
+	}
+
+	return nil
+}
