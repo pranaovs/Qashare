f79f178f73bdad2d5e0cd66bb0aa739d07c3faff
Pranaov S <git@pranaovs.me>
2026-01-28 18:29:05 +0530

    docs: swagger update instance url

--- server/docs/docs.go
+++ server/docs/docs.go
@@ -1308,7 +1308,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "devserver:9999",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Qashare API",
