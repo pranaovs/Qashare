12f6e5b32967341690983f2f789a7f0cd390b9dd
Pranaov S <git@pranaovs.me>
2026-02-05 10:30:06 +0530

    format dart code

--- client/lib/Config/api_config.dart
+++ client/lib/Config/api_config.dart
@@ -1,6 +1,9 @@
 class ApiConfig {
   static String _server = "https://qashare.pranaovs.me";
-  static const String _apiBasePath = String.fromEnvironment('API_BASE_PATH', defaultValue: '/api');
+  static const String _apiBasePath = String.fromEnvironment(
+    'API_BASE_PATH',
+    defaultValue: '/api',
+  );
 
   static String get baseUrl => "$_server$_apiBasePath/v1";
 
