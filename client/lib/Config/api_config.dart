ceccafe6adc91471955fb26aacc121edbc1a6ab4
Pranaov S <git@pranaovs.me>
2026-02-05 10:26:05 +0530

    feat: allow choosing api base path on frontend during building

--- client/lib/Config/api_config.dart
+++ client/lib/Config/api_config.dart
@@ -1,7 +1,8 @@
 class ApiConfig {
   static String _server = "https://qashare.pranaovs.me";
+  static const String _apiBasePath = String.fromEnvironment('API_BASE_PATH', defaultValue: '/api');
 
-  static String get baseUrl => "$_server";
+  static String get baseUrl => "$_server$_apiBasePath/v1";
 
   static void setServer(String server) {
     _server = server;
