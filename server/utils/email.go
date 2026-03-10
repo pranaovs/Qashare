5f56e405c18c8d5d6bb3488dec489280d59779f7
Pranaov S <git@pranaovs.me>
2026-03-10 18:29:20 +0530

    fix email mime

--- server/utils/email.go
+++ server/utils/email.go
@@ -37,7 +37,7 @@ func SendVerificationEmail(emailConfig config.EmailConfig, apiConfig config.APIC
 			"To: %s\r\n"+
 			"Subject: %s\r\n"+
 			"MIME-Version: 1.0\r\n"+
-			"Content-Type: text/html; charset=\"UTF-8\"\r\n",
+			"Content-Type: text/html; charset=\"UTF-8\"\r\n"+
 		"\r\n"+
 			"%s",
 		emailConfig.From, to, subject, body,
